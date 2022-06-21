4acc48d61c0f6d7d2a1cc5197881a581f357b179
wnandirta <nandira11@gmail.com>
2022-06-21 20:48:50 +0700

    Section 2

---
+++ src/config.js
@@ -0,0 +1,12 @@
+// config.js
+
+module.exports = {
+    apiKey: "AIzaSyBIqbBWseJODcW3q6dTFQttU2BNQz6Ltwg",
+    authDomain: "pfb-project.firebaseapp.com",
+    databaseURL: "https://pfb-project-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "pfb-project",
+    storageBucket: "pfb-project.appspot.com",
+    messagingSenderId: "800020160112",
+    appId: "1:800020160112:web:22dcd784fca50a1543deb5",
+    measurementId: "G-CJV8HB6VGK"
+};
--- src/index.js
+++ src/index.js
@@ -9,3 +9,42 @@ const {
 
 app.get('/todos', getAllTodos);
 exports.api = functions.https.onRequest(app);
+
+const {
+    loginUser
+} = require('./APIs/users')
+
+// Users
+app.post('/login', loginUser);
+
+const {
+    signUpUser
+} = require('./APIs/users')
+
+app.post('/signup', signUpUser);
+
+const auth = require('./util/auth');
+
+const {
+    uploadProfilePhoto
+} = require('./APIs/users')
+
+app.post('/user/image', auth, uploadProfilePhoto);
+
+const {
+    getUserDetail
+} = require('./APIs/users')
+
+app.get('/user', auth, getUserDetail);
+
+const {
+    updateUserDetails
+} = require('./APIs/users')
+
+app.post('/user', auth, updateUserDetails);
+
+app.get('/todos', auth, getAllTodos);
+app.get('/todo/:todoId', auth, getOneTodo);
+app.post('/todo',auth, postOneTodo);
+app.delete('/todo/:todoId',auth, deleteTodo);
+app.put('/todo/:todoId',auth, editTodo);
