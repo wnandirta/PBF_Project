094880897d08ecc1a68ab4a2f31e33411939fb27
wnandirta <nandira11@gmail.com>
2022-06-28 00:14:42 +0700

    crud todo

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import Login from "./Login";
 import Register from "./Register";
 import Reset from "./Reset";
 import Dashboard from "./Dashboard";
+import Title from "./Title";
+import AddTodo from "./AddTodo";
+import {
+  collection,
+  query,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "./firebase";
+
 
 function App() {
   return (
@@ -15,8 +27,53 @@ function App() {
           <Route exact path="/register" element={<Register />} />
           <Route exact path="/reset" element={<Reset />} />
           <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/addtodo" element={<AddTodo />} />
         </Routes>
       </Router>
+    </div>
+  );
+  const [todos, setTodos] = React.useState([]);
+
+  React.useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let todosArray = [];
+      querySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArray);
+    });
+    return () => unsub();
+  }, []);
+
+  const handleEdit = async (todo, title) => {
+    await updateDoc(doc(db, "todos", todo.id), { title: title });
+  };
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), { completed: !todo.completed });
+  };
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
+  return (
+    <div className="App">
+      <div>
+        <Title />
+      </div>
+      <div>
+        <AddTodo />
+      </div>
+      <div className="todo_container">
+        {todos.map((todo) => (
+          <Dashboard
+            key={todo.id}
+            todo={todo}
+            toggleComplete={toggleComplete}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,13 +1,65 @@
 import React from "react";
-
+import Title from "./Title";
+import AddTodo from "./AddTodo";
+import Todo from "./Todo";
 import "./Dashboard.css";
 import { logout } from "./firebase";
+import {
+  collection,
+  query,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "./firebase";
+
 
 function Dashboard() {
+  const [todos, setTodos] = React.useState([]);
 
+  React.useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let todosArray = [];
+      querySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArray);
+    });
+    return () => unsub();
+  }, []);
+
+  const handleEdit = async (todo, title) => {
+    await updateDoc(doc(db, "todos", todo.id), { title: title });
+  };
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), { completed: !todo.completed });
+  };
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
+  
   return (
     <div className="dashboard">
       <div className="dashboard__container">
+        <div>
+          <Title/>
+        </div>
+        <div>
+          <AddTodo/>
+        </div>
+        <div className="todo_container">
+          {todos.map((todo) => (
+            <Todo
+            key={todo.id}
+            todo={todo}
+            toggleComplete={toggleComplete}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+            />
+          ))}
+        </div>
         Logged in as
         <button className="dashboard__btn" onClick={logout}>
           Logout
